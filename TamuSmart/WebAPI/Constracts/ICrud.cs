1c26291cccf454d9aaa7b34e67dfa1dfd969f240
Muhammad Saefudin <Muhammad.Saefudin@rapidtech.id>
2024-09-16 20:34:23 +0700

    Add Interfaces

---
+++ TamuSmart/WebAPI/Constracts/ICrud.cs
@@ -0,0 +1,11 @@
+﻿namespace WebAPI.Constracts
+{
+    public interface ICrud <T> where T : class
+    {
+        IEnumerable<T> GetAll();
+        T GetByID(int id);
+        T Add(T entity);
+        T Update(T entity);
+        T Delete(T entity);
+    }
+}
---
+++ TamuSmart/WebAPI/Constracts/IEvent.cs
@@ -0,0 +1,11 @@
+﻿using WebAPI.Models;
+
+namespace WebAPI.Constracts
+{
+    public interface IEvent : ICrud<Event>
+    {
+        IEnumerable<Event> GetEventsByName(string eventName);
+        IEnumerable<Event> GetEventsByDate(string eventDate);
+        IEnumerable<Event> GetEventsByLocation(string eventLocation);
+    }
+}
